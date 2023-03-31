fadd427ba6871efc4d285111f75d0bbb62148d0c
Hans Nudding <hansndt98@gmail.com>
2023-03-31 03:51:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = os.environ['MYSQL_HOST']
 app.config['MYSQL_USER'] = os.environ['MYSQL_USER']
-app.config['MYSQL_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+app.config['MYSQL_DATABASE_URL'] = os.environ['MYSQL_URL']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
+app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 app.config['MYSQL_DB'] = os.environ['MYSQL_DB']
 
 # Ruta index (landing page)
@@ -18,10 +20,7 @@ def index():
 @app.route('/contactos')
 def contactos():
     conn = pymysql.connect(
-        host=app.config['MYSQL_HOST'],
-        user=app.config['MYSQL_USER'],
-        password=app.config['MYSQL_PASSWORD'],
-        db=app.config['MYSQL_DB'],
+        url=app.config['MYSQL_DATABASE_URL'],
         cursorclass=pymysql.cursors.DictCursor
     )
     cur = conn.cursor()
