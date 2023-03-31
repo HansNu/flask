b03b6047f54062c9b07db03c5c496b1110b060bc
Hans Nudding <hansndt98@gmail.com>
2023-03-31 02:29:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+mysql = MySQL(app)
 
+## Configuracion de la BD
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'nmAdmin'
+app.config['MYSQL_PASSWORD'] = '123'
+app.config['MYSQL_DB'] = 'NuncaMas'
 
-@app.route('/')
+# Ruta index (landing page)
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')
 
+# Ruta contactos (organizaciones y sus contactos)
+@app.route('/contactos')
+def contactos():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM organizacion")
+    orgs = cur.fetchall()
+    cur.connection.commit()
+    cur.close()
+    print(orgs)
+    return render_template('contactanos.html', orgs=orgs)
+
+# Ruta informate
+@app.route('/informate')
+def informate():
+    return render_template('informate.html')
+
+# Ruta test
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
