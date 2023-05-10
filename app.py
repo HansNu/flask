92a8e1f0932093329fca0e3798c18ffdc95396b2
Hans Nudding <hansndt98@gmail.com>
2023-05-10 17:04:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,52 @@
 from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
+import pymysql
 import os
 
 app = Flask(__name__)
 
 # MySQL configurations
-app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
-app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT'))  # make sure the port is an integer
-mysql = MySQL(app)
+db_config = {
+    'host': os.getenv('MYSQLHOST'),
+    'user': os.getenv('MYSQLUSER'),
+    'password': os.getenv('MYSQLPASSWORD'),
+    'db': os.getenv('MYSQLDATABASE'),
+    'port': int(os.getenv('MYSQLPORT'))  # make sure the port is an integer
+}
 
-# Ruta index (landing page)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
-# Ruta contactos (organizaciones y sus contactos)
 @app.route('/contactos', methods=['GET', 'POST'])
 def contactos():
-    contactos = []
-    cur = mysql.connection.cursor()
-    cur.execute("select contacto.idContacto, contacto.nombre, apellido, contacto.telefono, contacto.correo, organizacion.nombre, contactoOrg.rol from contacto, contactoOrg, organizacion where contacto.idContacto = contactoOrg.idContacto and contactoOrg.idOrganizacion = organizacion.idOrganizacion")
-    contactos = cur.fetchall()
-    cur.connection.commit()
-    cur.close()
-    print(contactos)
+    connection = pymysql.connect(**db_config)
+    with connection.cursor() as cursor:
+        cursor.execute("select contacto.idContacto, contacto.nombre, apellido, contacto.telefono, contacto.correo, organizacion.nombre, contactoOrg.rol from contacto, contactoOrg, organizacion where contacto.idContacto = contactoOrg.idContacto and contactoOrg.idOrganizacion = organizacion.idOrganizacion")
+        contactos = cursor.fetchall()
+    connection.close()
     return render_template('contactos.html', contactos=contactos)
 
 @app.route('/organizaciones', methods=['GET', 'POST'])
 def organizaciones():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM organizacion")
-    orgs = cur.fetchall()
-    cur.connection.commit()
-    cur.close()
-
+    connection = pymysql.connect(**db_config)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM organizacion")
+        orgs = cursor.fetchall()
+    connection.close()
     return render_template('organizaciones.html', orgs=orgs)
 
-# Ruta informate
 @app.route('/informate')
 def informate():
     return render_template('informate.html')
 
-# Ruta test
 @app.route('/test')
 def test():
     return render_template('test.html')
 
-# Ruta para responder el test
 @app.route('/responderTest')
 def responder():
     return render_template('responderTest.html')
 
-# Ruta para mostrar resultados
 @app.route('/resultados', methods=['POST', 'GET'])
 def resultados():
     if request.method=='GET':
@@ -80,7 +72,6 @@ def resultados():
         #### Falta crear consulta para encontrar organizaciones apropiadas
 
         return render_template('resultados.html', resultado=resultado)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
