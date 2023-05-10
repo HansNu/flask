14290495a63c89e89675837ac29ad20048503aa7
Hans Nudding <hansndt98@gmail.com>
2023-05-10 16:47:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request
-import pymysql
+from flask_mysqldb import MySQL
 import os
 
 app = Flask(__name__)
 
-mysql_host = os.environ.get('MYSQLHOST')
-mysql_port = os.environ.get('MYSQLPORT')
-mysql_user = os.environ.get('MYSQLUSER')
-mysql_password = os.environ.get('MYSQLPASSWORD')
-mysql_database = os.environ.get('MYSQLDATABASE')
+# MySQL configurations
+app.config['MYSQL_HOST'] = os.getenv('MYSQLHOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQLUSER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQLPASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQLDATABASE')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQLPORT'))  # make sure the port is an integer
+mysql = MySQL(app)
 
 # Ruta index (landing page)
 @app.route('/', methods=['GET', 'POST'])
@@ -16,23 +18,26 @@ def index():
     return render_template('index.html')
 
 # Ruta contactos (organizaciones y sus contactos)
-@app.route('/contactos')
+@app.route('/contactos', methods=['GET', 'POST'])
 def contactos():
-    conn = pymysql.connect(
-        host=mysql_host,
-        port=int(mysql_port),
-        user=mysql_user,
-        password=mysql_password,
-        db=mysql_database,
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
-    cur = conn.cursor()
+    contactos = []
+    cur = mysql.connection.cursor()
+    cur.execute("select contacto.idContacto, contacto.nombre, apellido, contacto.telefono, contacto.correo, organizacion.nombre, contactoOrg.rol from contacto, contactoOrg, organizacion where contacto.idContacto = contactoOrg.idContacto and contactoOrg.idOrganizacion = organizacion.idOrganizacion")
+    contactos = cur.fetchall()
+    cur.connection.commit()
+    cur.close()
+    print(contactos)
+    return render_template('contactos.html', contactos=contactos)
+
+@app.route('/organizaciones', methods=['GET', 'POST'])
+def organizaciones():
+    cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM organizacion")
     orgs = cur.fetchall()
-    conn.close()
-    print(orgs)
-    return render_template('contactanos.html', orgs=orgs)
+    cur.connection.commit()
+    cur.close()
+
+    return render_template('organizaciones.html', orgs=orgs)
 
 # Ruta informate
 @app.route('/informate')
@@ -43,6 +48,39 @@ def informate():
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+# Ruta para responder el test
+@app.route('/responderTest')
+def responder():
+    return render_template('responderTest.html')
+
+# Ruta para mostrar resultados
+@app.route('/resultados', methods=['POST', 'GET'])
+def resultados():
+    if request.method=='GET':
+        return "NO PUEDES ENTRAR AQUI ASI"
+    else:
+        total = 0
+        # Con este nivel de violencia se buscan organizaciones apropiadas
+        nivel = 0
+        for key in request.form:
+            total = total + int(request.form[key])
+        print(total)
+        resultado = ""
+        if total <= 20:
+            resultado = "No se detectan indicios claros de violencia en la relación. Aun así, se recomienda estar atenta y buscar apoyo si sientes que algo no está bien."
+            nivel = 1
+        elif total <= 40:
+            resultado = "Se identifican señales de violencia en la relación. Es importante buscar apoyo y recursos para enfrentar la situación."
+            nivel = 2
+        elif total > 40:
+            resultado = "Se detecta una situación de violencia grave en la relación. Es urgente buscar ayuda y protección."
+            nivel = 3
+
+        #### Falta crear consulta para encontrar organizaciones apropiadas
+
+        return render_template('resultados.html', resultado=resultado)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
